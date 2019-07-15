4a1b01ca413a634376e0a72052f0f7399a42f340
Jason Veizaj <jason.veizaj@gmail.com>
2019-07-15 11:50:51 -0400

    Update and rename trie.py to node.py

--- node.py
+++ node.py
@@ -1,13 +1,13 @@
-"""Implements trie class. Loads dictionary into memory via trie data structure,
+"""Implements a trie node. Loads dictionary into memory via trie data structure,
 navigates through trie to check spelling, uses trie to suggest similar words.
 Writes to output file."""
 
 
-class Trie(object):
+class Node(object):
     def __init__(self):
         # instance variables
         i = 0
 
-    # Returns True if trie is successfully loaded into memory. Otherwise, returns False
+    # Returns true if trie is successfully loaded into memory. Otherwise, return true
     def status(self):
         return False
