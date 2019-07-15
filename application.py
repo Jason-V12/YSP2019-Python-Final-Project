1bcda3e1facd3c12960aba61c721047bcfd5119c
Jason Veizaj <jason.veizaj@gmail.com>
2019-07-15 18:34:36 -0400

    Update application.py

--- application.py
+++ application.py
@@ -1,9 +1,30 @@
 """SpellChecker Main file - creates instances of classes and calls methods from 
 helper files"""
 
-from trie import *
+from node import *
 from interface import *
 
+# @param {}
+# @return {string} dictionary if successful, else {boolean} False
+# Reads in dictionary requested from user input
+def read_dict()
+    # TODO: handle different dictionaries via options in interface.py
+    dict_file = open("EnglishDictionary.txt", "r")
+    dict_in = dict_file.read()
+    if dict_in == "":
+        return False
+    else:
+        return dict_in
+
+# @param {string} dictionary
+# @return {boolean} success_value
+# Loads dictionary into memory, uses node objects to construct complete trie
+def trie(dictionary):
+
 if __name__ == "__main__":
-    trie_instance = trie.Trie()
-    print("Completed runtime")
+    dictionary = read_dict()
+    if not dictionary:
+        exit(1)
+    else:
+        trie(dictionary)
+print("Completed runtime")
