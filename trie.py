eea5d7d01a63a7c7b52031d86880888133ec6113
Jason Veizaj <jason.veizaj@gmail.com>
2019-07-15 10:44:16 -0400

    Create trie.py

---
+++ trie.py
@@ -0,0 +1,13 @@
+"""Implements trie class. Loads dictionary into memory via trie data structure,
+navigates through trie to check spelling, uses trie to suggest similar words.
+Writes to output file."""
+
+
+class Trie(object):
+    def __init__(self):
+        # instance variables
+        i = 0
+
+    # Returns true if trie is successfully loaded into memory. Otherwise, return true
+    def status(self):
+        return False
