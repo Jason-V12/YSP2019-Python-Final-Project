1c0da2965101891f10c626fca4176ed4a428b4ed
Jason Veizaj <jason.veizaj@gmail.com>
2019-07-15 11:49:04 -0400

    Create diagnostics.py

---
+++ diagnostics.py
@@ -0,0 +1,18 @@
+"""Calculates diagnostic values and outputs to diagnostic file. Details application execution."""
+
+class Diagnostics(object):
+    def __init__(self):
+        #instance variables
+        i = 0
+
+    def miss_word_count(self):
+
+    def dict_word_count(self):
+
+    def dict_name(self):
+
+    def file_name(self):
+
+    def run_time(self):
+
+    def time_calculate(self):
