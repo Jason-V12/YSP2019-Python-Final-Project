027e894a74478c4215f32f3cf914028ab61151eb
srikarg19 <52252149+srikarg19@users.noreply.github.com>
2019-07-16 19:00:20 -0400

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,4 +1,3 @@
-"""Creates a graphical user interface and handles input."""
 from tkinter import *
 from tkinter.filedialog import askopenfilename
 
@@ -16,34 +15,34 @@ class DrawInterface(object):
         self.choosefileimage = PhotoImage(file = "choosefile.gif")
         self.exitimage = PhotoImage(file = "exitimage.gif")
         self.browseimage = PhotoImage(file = "browseimage.gif")
+        self.chosenimage = PhotoImage(file = "filechosen.gif")
+        self.restartimage = PhotoImage(file = "restartimage.gif")
         self.file = None
     #param (none)
     def make_interface(self):
         exitbtn = Button(self.top, height = 40, width = 100, image = self.exitimage, command = self.exit_interface)
         exitbtn.place(x = 300, y = 560)
-        filelbl = Label(self.top, height = 55, width = 150, image = self.choosefileimage)
-        filelbl.place(x = 0, y = -5)
-        filebtn = Button(self.top, height = 40, width = 75, image = self.browseimage, command = self.upload_file)
-        filebtn.place(x = 150, y = 0)
-        #restartbtn = Button(self.top, height = 40, width = 100, text = "Restart", command = self.make_interface)
-        #restartbtn.place(x = 300, y = 0)
+        filelbl = Label(self.top, height = 40, width = 150, image = self.choosefileimage)
+        filelbl.place(x = 0, y = 0)
+        fileChoosebtn = Button(self.top, height = 40, width = 75, image = self.browseimage, command = self.upload_file)
+        fileChoosebtn.place(x = 155, y = 0)
+        restartbtn = Button(self.top, height = 40, width = 100, image = self.restartimage, command = self.make_interface)
+        restartbtn.place(x = 300, y = 0)
     #param (none)
     def exit_interface(self):
         self.top.destroy()
     #param (none)
     def upload_file(self):
-        while self.file == None:
-            self.file = askopenfilename(title = "Choose a file:")
+        self.file = askopenfilename(title = "Choose a file:", filetypes = (("Text File", "*.txt"), ("PDF File","*.pdf"), ("Microsoft Word Document", "*.doc*")))
         try:
-            with open(self.file, "r") as UseFile:
-                filetext = self.file.read()
-                self.file.close()
+            filetext = open(self.file)
+            filetext.read()
+            filetext.close()
+            filelbl = Label(self.top, height = 40, width = 150, image = self.chosenimage)
+            filelbl.place(x = 0, y = 0)
         except:
-            self.file = None
-            print("No such file exists")
-
+            print("Try again")
+        print(filetext)
 
 x = DrawInterface()
 x.make_interface()
-
-# Note: PyQt or Tkinter
