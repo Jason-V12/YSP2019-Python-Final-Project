5242fec60eaec3ebe44f9e576f3463b076ad54ea
Jason Veizaj <jason.veizaj@gmail.com>
2019-07-20 22:12:17 -0400

    Update trie.py

--- trie.py
+++ trie.py
@@ -4,31 +4,42 @@ Writes to output file."""
 
 from dataclasses import dataclass
 from time import time
+from copy import deepcopy
+
 
 # Struct-like class containing empty string array of children and boolean representing if word
-@dataclass
-class Node:
-    children = [None] * 41
-    is_word = False
+class Node(object):
+    def __init__(self):
+        self.children = [None] * 41
+        self.is_word = False
 
 
+# Class implementing complete functionality of a trie
 class Trie(object):
     def __init__(self, dict_in_use=None):
         # instance variables
-        self.head_node = Node()
+        self.parent_node = Node()
+        self.traversal_node = self.parent_node
         self.dict_in_use = dict_in_use
         self.word_count = 0
         self.INDEX = 0
+        self.new_node = Node()
 
+    # @param {}
+    # @return {boolean} True if successful, else {boolean} False
+    # Checks to make sure dictionary is passed in
     def check_dict(self):
         if self.dict_in_use is None:
             return False
         else:
             return True
 
+    # @param {}
+    # @return {boolean} True if successful
+    # Loads dictionary into Trie data structure
     def add_words(self):
         start_time = time()
-        traversal_node = self.head_node  # copy.copy()
+        self.traversal_node = self.parent_node
         for cursor in range(0, len(self.dict_in_use)):
             if self.dict_in_use[cursor] == "\'":
                 self.INDEX = 26
@@ -42,64 +53,68 @@ class Trie(object):
                 self.INDEX = 30
             elif self.dict_in_use[cursor].isalpha():
                 self.INDEX = ord(self.dict_in_use[cursor].lower()) - 97
-            else:
+            elif self.dict_in_use[cursor].isnumeric():
                 self.INDEX = ord(self.dict_in_use[cursor]) - 17
-
-            if self.dict_in_use[cursor] == "\n":
-                traversal_node.is_word = True
-                traversal_node = self.head_node  # copy.copy()
-                self.word_count = self.word_count + 1
-            elif traversal_node.children[self.INDEX] is None:
-                traversal_node.children[self.INDEX] = Node()
-                traversal_node = traversal_node.children[self.INDEX]
             else:
-                traversal_node = traversal_node.children[self.INDEX]
+                pass
+            if self.dict_in_use[cursor] == "\n":
+                self.traversal_node.is_word = True
+                self.traversal_node = self.parent_node  # Point traversal node back to head node
+                self.word_count = self.word_count + 1
+            elif self.traversal_node.children[self.INDEX] is None:
+                self.traversal_node.children[self.INDEX] = deepcopy(self.new_node)
+                self.traversal_node = self.traversal_node.children[self.INDEX]
+            else:
+                self.traversal_node = self.traversal_node.children[self.INDEX]
         print("Dictionary load time in seconds: " + str(time() - start_time))
+        print(self.word_count)
         return True
 
+    # @param {string} word
+    # @return {boolean} input_file if successful, else {boolean} False
+    # Reads in file from file location specified by user, converts file to .txt if applicable
     def check_word(self, word):
         word = str(word)
-        traversal_node = self.head_node  # copy.copy()
+        self.traversal_node = self.parent_node
         lower_word = [word[i].lower() for i in range(0, len(word))]
         for letter in range(0, len(lower_word)):
             if lower_word[letter] == "\'":
-                if traversal_node.children[26] is not None:
-                    traversal_node = traversal_node.children[26]
+                if self.traversal_node.children[26] is not None:
+                    self.traversal_node = self.traversal_node.children[26]
                 else:
                     return False
             elif lower_word[letter] == "&":
-                if traversal_node.children[27] is not None:
-                    traversal_node = traversal_node.children[27]
+                if self.traversal_node.children[27] is not None:
+                    self.traversal_node = self.traversal_node.children[27]
                 else:
                     return False
             elif lower_word[letter] == "-":
-                if traversal_node.children[28] is not None:
-                    traversal_node = traversal_node.children[28]
+                if self.traversal_node.children[28] is not None:
+                    self.traversal_node = self.traversal_node.children[28]
                 else:
                     return False
             elif lower_word[letter] == ".":
-                if traversal_node.children[29] is not None:
-                    traversal_node = traversal_node.children[29]
+                if self.traversal_node.children[29] is not None:
+                    self.traversal_node = self.traversal_node.children[29]
                 else:
                     return False
             elif lower_word[letter] == "/":
-                if traversal_node.children[30] is not None:
-                    traversal_node = traversal_node.children[30]
+                if self.traversal_node.children[30] is not None:
+                    self.traversal_node = self.traversal_node.children[30]
                 else:
                     return False
             elif lower_word[letter].isalpha():
-                if traversal_node.children[ord(lower_word[letter]) - 97] is not None:
-                    traversal_node = traversal_node.children[ord(lower_word[letter]) - 97]
+                if self.traversal_node.children[ord(lower_word[letter]) - 97] is not None:
+                    self.traversal_node = self.traversal_node.children[ord(lower_word[letter]) - 97]
                 else:
                     return False
-            else:
-                if traversal_node.children[ord(lower_word[letter]) - 17] is not None:
-                    traversal_node = traversal_node.children[ord(lower_word[letter]) - 17]
+            elif lower_word[letter].isnumeric():
+                if self.traversal_node.children[ord(lower_word[letter]) - 17] is not None:
+                    self.traversal_node = self.traversal_node.children[ord(lower_word[letter]) - 17]
                 else:
                     return False
             if letter == len(lower_word) - 1:
-                if traversal_node.is_word:
+                if self.traversal_node.is_word is True:
                     return True
                 else:
                     return False
-
