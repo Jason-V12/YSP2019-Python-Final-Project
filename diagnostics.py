aa9d2bdaf2138796d9bb4d38cb6ac52743dc8fed
AVustepalli <52898839+AVustepalli@users.noreply.github.com>
2019-07-21 17:21:55 -0400

    Update diagnostics.py

--- diagnostics.py
+++ diagnostics.py
@@ -1,18 +1,29 @@
 """Calculates diagnostic values and outputs to diagnostic file. Details application execution."""
 
+import timeit
 class Diagnostics(object):
     def __init__(self):
         #instance variables
         i = 0
 
     def miss_word_count(self):
-
-    def dict_word_count(self):
-
-    def dict_name(self):
-
-    def file_name(self):
-
-    def run_time(self):
-
-    def time_calculate(self):
+        return True
+    def dict_word_count(self, fileName):
+        file = open(fileName, 'r')
+        f = file.read()
+        words = f.split()
+        count = len(words)
+        f.close()
+        return count
+    def dict_name(self, dicLocation):
+        index = repr(dicLocation.rfind('\\')) + 1 
+        dicName = dicLocation[index::]
+        return dicName
+    def file_name(self, fileLocation):
+        index = repr(fileLocation.rfind('\\')) + 1 
+        fileName = fileLocation[index::]
+        return fileName
+    def get_time(self):
+        return timeit.default_timer()
+    def time_calculate(self, start, end):
+        return (end - start)
